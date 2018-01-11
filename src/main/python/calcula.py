ab817fd8780816c736f1bb1f161493d96a0fe0e9
Flamarion Jorge <flamarion.jorge@nike.com>
2018-01-11 20:32:02 +0100

    Creating the project structure

--- src/main/python/calcula.py
+++ src/main/python/calcula.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+
 class Calcula(object):
     def __init__(self, a, b):
         self.a = a
         self.b = b
+
     def soma(self):
         return self.a + self.b
--- src/unittest/python/test_calcula.py
+++ src/unittest/python/test_calcula.py
@@ -1,16 +1,20 @@
 import unittest
-from calcula import Calcula
+from src.main.python.calcula import Calcula
 
 
 class TestSoma(unittest.TestCase):
+
     def setUp(self):
         pass
+
     def test_soma_2_2(self):
-        x = Calcula(2,2)
+        x = Calcula(2, 2)
         self.assertTrue(x.soma() == 4)
+
     def teste_soma_3_3_fail(self):
-        x = Calcula(3,3)
+        x = Calcula(3, 3)
         self.assertFalse(x.soma() == 5)
+
 
 if __name__ == '__main__':
     unittest.main()
