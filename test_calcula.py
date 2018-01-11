ab9df89ed45ecc054f5f5a1a428ea7f4aa22cbdc
Flamarion Jorge <flamarion.jorge@nike.com>
2018-01-11 20:15:42 +0100

    Some adjustments around the tests

--- test_calcula.py
+++ test_calcula.py
@@ -7,10 +7,10 @@ class TestSoma(unittest.TestCase):
         pass
     def test_soma_2_2(self):
         x = Calcula(2,2)
-        self.assertEqual(x.soma(), 4)
-    def teste_soma_2_2_fail(self):
-        x = Calcula(2,2)
-        self.assertIsNot(x.soma(), 5)
+        self.assertTrue(x.soma() == 4)
+    def teste_soma_3_3_fail(self):
+        x = Calcula(3,3)
+        self.assertFalse(x.soma() == 5)
 
 if __name__ == '__main__':
     unittest.main()
