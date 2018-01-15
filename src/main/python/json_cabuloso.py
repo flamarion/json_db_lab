84f675a659906a1772d9683742f9cd874c8c7d90
Flamarion Jorge <flamarion.jorge@nike.com>
2018-01-15 16:42:46 +0100

    Fucking huge changes in this piece of shit. Enjoy

--- src/main/python/json_cabuloso.py
+++ src/main/python/json_cabuloso.py
@@ -1,30 +1,47 @@
 import base64
-import ast
+import json
 
 '''
 This class will decrypt the file content and replace key_replacement by lucio
 It receive a json file with content encrypted using base64
+eyAiYnVjZXRhIiA6ICJjdSIgfQ==
 '''
 
 
 class JsonCabuloso:
 
-    def __init__(self, file, replace_key):
-        self.file = file
-        self.replace_key = replace_key
+    def __init__(self, sample_file, new_file, key_ori, key_rep):
+        self.sample_file = sample_file
+        self.new_file = new_file
+        self.key_ori = key_ori
+        self.key_rep = key_rep
 
-    def change_json(self):
-        with open(self.file, 'r') as f:
-            a = f.read()
+    def _base64ToString(self, string_encoded):
+        decoded_string = base64.b64decode(string_encoded).decode('UTF-8')
+        return decoded_string
 
-        d = ast.literal_eval(base64.b64decode(a).decode('UTF-8'))
+    def _stringToBase64(self, some_string):
+        byte_array = base64.b64encode(some_string.encode('UTF-8'))
+        encoded_string = byte_array.decode("utf-8")
+        return encoded_string
 
-        for key in d.keys():
-            if key == self.replace_key:
-                d['lucio'] = d.pop(key)
-                e = base64.b64encode(str(d).encode('UTF-8'))
-                with open(self.file, 'w') as f:
-                    f.write(str(e).strip('b\''))
-                return True
-            else:
-                return False
+    def read_json(self):
+        with open(self.sample_file, 'r') as f:
+            json_string = f.read()
+        decoded_json = json.loads(self._base64ToString(json_string))
+        return decoded_json
+
+    def change_json(self, decoded_json):
+        for key in decoded_json.keys():
+            if key == self.key_ori:
+                decoded_json[self.key_rep] = decoded_json.pop(self.key_ori)
+            new_json = json.dumps(decoded_json)
+            encoded_json = self._stringToBase64(new_json)
+            with open(self.new_file, 'w') as f:
+                f.write(encoded_json)
+                return encoded_json
+
+
+x = JsonCabuloso("sample.json", "novo_json", "buceta", "caceta")
+b = (x.read_json())
+print(x.change_json(b))
--- src/unittest/python/json_cabuloso_tests.py
+++ src/unittest/python/json_cabuloso_tests.py
@@ -2,20 +2,5 @@ import unittest
 from json_cabuloso import JsonCabuloso
 
 
-class TestKeyReplace(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def test_replace_flamarion_lucio(self):
-        i = JsonCabuloso(file='python/test1.json', replace_key='flamarion')
-        result = i.change_json()
-        self.assertTrue(result)
-
-    def test_replace_key_not_found(self):
-        i = JsonCabuloso(file='python/test2.json', replace_key='flamarion')
-        result = i.change_json()
-        self.assertFalse(result)
-
-
-if __name__ == '__main__':
-    unittest.main()
+class TestJsonCabuloso(unittest.TestCase):
+    pass
