200f7fc3891e1f6b42133e19701ec77b48daa3e7
Flamarion Jorge <flamarion.jorge@nike.com>
2018-01-16 23:21:30 +0100

    Exceptions looks better.

--- src/main/python/json_cabuloso.py
+++ src/main/python/json_cabuloso.py
@@ -11,8 +11,14 @@ class JsonCabuloso:
         self.decoded_json = json.loads(utils._base64ToString(utils._read(self.sample_file)))
 
     def change(self, key_ori, new_value):
-        if key_ori in self.decoded_json:
-            self.decoded_json[key_ori] = new_value
+        try:
+            if key_ori not in self.decoded_json:
+                print("Key {key} not found for changing".format(key=key_ori))
+                raise KeyError
+            else:
+                self.decoded_json[key_ori] = new_value
+        except KeyError:
+            raise
 
     def add(self, new_key, new_value):
         self.decoded_json[new_key] = new_value
@@ -27,14 +33,28 @@ class JsonCabuloso:
 
     def delete(self, key):
         try:
-            assert key in self.decoded_json
-            if key in self.decoded_json:
-                self.decoded_json.pop(key)
-        except AssertionError:
-            print("Key {key} not found".format(key=key))
-            raise
+            self.decoded_json.pop(key)
+        except KeyError:
+            print("Key {key} not found for deletion".format(key=key))
+            pass
 
     def save(self):
         encoded_json = utils._stringToBase64(json.dumps(self.decoded_json))
         with open(self.sample_file, 'w') as f:
             f.write(encoded_json)
+
+
+x = JsonCabuloso("sample.json")
+x.retrieveAll()
+x.add("Lucio", "Africano")
+print("==========")
+x.retrieveAll()
+print("==========")
+x.change("Flamarion", "Polaco")
+x.retrieveAll()
+print("==========")
+x.delete("Flamarion")
+x.delete("Ragul")
+x.delete("xpto")
+x.change("Teste", "Nada")
+x.retrieveAll()
