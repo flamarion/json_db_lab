9fa912d9c49ca28f51e3e770f31565e850b7e0ad
Flamarion Jorge <flamarion.jorge@nike.com>
2018-01-16 22:10:38 +0100

    Starting to play with exceptions.

--- src/main/python/json_cabuloso.py
+++ src/main/python/json_cabuloso.py
@@ -1,51 +1,40 @@
-import base64
+#!/usr/bin/env python
+
 import json
-
-"""
-Class read a json file with content encoded using base64, replace the values of keys 
-and update the file.
-
-Ex.:
-
-x = JsonCabuloso("sample.json")
-x.change("Key1", "NewValue1")
-x.change("Key2", "NewValue2")
-x.save
-
-"""
+import utils
 
 
 class JsonCabuloso:
 
     def __init__(self, sample_file):
         self.sample_file = sample_file
-        self.decoded_json = json.loads(self._base64ToString(self._read()))
-
-    def _base64ToString(self, string_encoded):
-        decoded_string = base64.b64decode(string_encoded).decode('UTF-8')
-        return decoded_string
-
-    def _stringToBase64(self, some_string):
-        byte_array = base64.b64encode(some_string.encode('UTF-8'))
-        encoded_string = byte_array.decode("utf-8")
-        return encoded_string
-
-    def _read(self):
-        with open(self.sample_file, 'r') as f:
-            json_string = f.read()
-        return json_string
+        self.decoded_json = json.loads(utils._base64ToString(utils._read(self.sample_file)))
 
     def change(self, key_ori, new_value):
         if key_ori in self.decoded_json:
             self.decoded_json[key_ori] = new_value
 
+    def add(self, new_key, new_value):
+        self.decoded_json[new_key] = new_value
+
+    def retrieve(self, key):
+        if key in self.decoded_json:
+            print("The value of {key} is {value}".format(key=key, value=self.decoded_json[key]))
+
+    def retrieveAll(self):
+        for key, value in self.decoded_json.items():
+            print("The value of {key} is {value}".format(key=key, value=self.decoded_json[key]))
+
+    def delete(self, key):
+        try:
+            assert key in self.decoded_json
+            if key in self.decoded_json:
+                self.decoded_json.pop(key)
+        except AssertionError:
+            print("Key {key} not found".format(key=key))
+            raise
+
     def save(self):
-        encoded_json = self._stringToBase64(json.dumps(self.decoded_json))
+        encoded_json = utils._stringToBase64(json.dumps(self.decoded_json))
         with open(self.sample_file, 'w') as f:
             f.write(encoded_json)
-
-
-x = JsonCabuloso("sample.json")
-x.change("Ragul", "Brazilian")
-x.change("Flamarion", "Indian")
-x.save()
---
+++ src/main/python/utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import base64
+
+
+def _base64ToString(string_encoded):
+    decoded_string = base64.b64decode(string_encoded).decode('UTF-8')
+    return decoded_string
+
+
+def _stringToBase64(some_string):
+    byte_array = base64.b64encode(some_string.encode('UTF-8'))
+    encoded_string = byte_array.decode("utf-8")
+    return encoded_string
+
+
+def _read(sample_file):
+    with open(sample_file, 'r') as f:
+        json_string = f.read()
+    return json_string
