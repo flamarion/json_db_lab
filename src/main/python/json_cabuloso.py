0bcb1dcd89074128f54a3c01c9cfa68dab059ca9
Flamarion Jorge <flamarion.jorge@nike.com>
2018-01-16 15:42:52 +0100

    Fuck this shit... is getting better.

--- src/main/python/json_cabuloso.py
+++ src/main/python/json_cabuloso.py
@@ -1,36 +1,25 @@
 import base64
 import json
 
-'''
-This class accept 4 parameters
-1 - json file to bechanged ("sample.json" on example)
-2 - new json file to be created ("novo.json" on the example)
-3 - key to be replaced ("buceta" on the example)
-4 - key to replace ("caceta" on the example)
+"""
+Class read a json file with content encoded using base64, replace the values of keys 
+and update the file.
 
-The content of the "sample.json" is
+Ex.:
 
-eyAiYnVjZXRhIiA6ICJjdSIgfQ== ( {"buceta" : "cu" })
+x = JsonCabuloso("sample.json")
+x.change("Key1", "NewValue1")
+x.change("Key2", "NewValue2")
+x.save
 
-Example:
-
-If you want to replace "buceta" by "caceta":
-
-x = JsonCabuloso("sample.json", "novo.json", "buceta", "caceta")
-b = (x.read_json())
-x.change_json(b)
-
-
-'''
+"""
 
 
 class JsonCabuloso:
 
-    def __init__(self, sample_file, new_file, key_ori, key_rep):
+    def __init__(self, sample_file):
         self.sample_file = sample_file
-        self.new_file = new_file
-        self.key_ori = key_ori
-        self.key_rep = key_rep
+        self.decoded_json = json.loads(self._base64ToString(self._read()))
 
     def _base64ToString(self, string_encoded):
         decoded_string = base64.b64decode(string_encoded).decode('UTF-8')
@@ -41,18 +30,22 @@ class JsonCabuloso:
         encoded_string = byte_array.decode("utf-8")
         return encoded_string
 
-    def read_json(self):
+    def _read(self):
         with open(self.sample_file, 'r') as f:
             json_string = f.read()
-        decoded_json = json.loads(self._base64ToString(json_string))
-        return decoded_json
+        return json_string
 
-    def change_json(self, decoded_json):
-        for key in decoded_json.keys():
-            if key == self.key_ori:
-                decoded_json[self.key_rep] = decoded_json.pop(self.key_ori)
-            new_json = json.dumps(decoded_json)
-            encoded_json = self._stringToBase64(new_json)
-            with open(self.new_file, 'w') as f:
-                f.write(encoded_json)
-                return encoded_json
+    def change(self, key_ori, new_value):
+        if key_ori in self.decoded_json:
+            self.decoded_json[key_ori] = new_value
+
+    def save(self):
+        encoded_json = self._stringToBase64(json.dumps(self.decoded_json))
+        with open(self.sample_file, 'w') as f:
+            f.write(encoded_json)
+
+
+x = JsonCabuloso("sample.json")
+x.change("Ragul", "Brazilian")
+x.change("Flamarion", "Indian")
+x.save()
