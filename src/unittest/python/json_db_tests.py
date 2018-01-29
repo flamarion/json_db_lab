e2d86f5cdbb4bc8e56f186821a430a44bcd9dc3a
Flamarion Jorge <flamarion.jorge@nike.com>
2018-01-29 16:33:02 +0100

    Improving the tests.

--- src/unittest/python/json_db_tests.py
+++ src/unittest/python/json_db_tests.py
@@ -1,13 +1,46 @@
 import unittest
+import os
 from json_db import JsonDbHandler, MyException
 
 
 class TestJsonDb(unittest.TestCase):
-    def setUp(self):
-        self.op = JsonDbHandler("/Users/fjorg1/Labs/mentoring/src/unittest/python/sample.json")
 
-    def test_success_add(self):
-        self.assertRaises(MyException, self.op.add("Y", "Y"))
+    def setUp(self):
+        my_dir = os.path.dirname(__file__)
+        sample_file = os.path.join(my_dir, "sample.json")
+        self.op = JsonDbHandler(sample_file)
+
+    def test_add(self):
+        self.op.add("X", "Y")
+
+    def test_add_fail(self):
+        self.op.add("X", "Y")
+        with self.assertRaises(MyException):
+            self.op.add("X", "Y")
+
+    def test_delete(self):
+        self.op.delete("Flamarion")
+
+    def test_delete_fail(self):
+        with self.assertRaises(MyException):
+            self.op.delete("Z")
+
+    def test_change(self):
+        self.op.change("Flamarion", "Test")
+
+    def test_change_fail(self):
+        with self.assertRaises(MyException):
+            self.op.change("Z", "Y")
+
+    def test_query(self):
+        self.op.query("Flamarion")
+
+    def test_query_fail(self):
+        with self.assertRaises(MyException):
+            self.op.query("Z")
+
+    def test_query_all(self):
+        self.op.retrieve_all()
 
 
 if __name__ == '__main__':
