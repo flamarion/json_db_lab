795f9145659e4b0dc8fa76992051bfef1303cdfb
Flamarion Jorge <flamarion.jorge@nike.com>
2018-01-30 16:10:32 +0100

    Finishing the unittests

--- build.py
+++ build.py
@@ -6,13 +6,23 @@ use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
-use_plugin('python.pycharm')
+use_plugin("python.pycharm")
+use_plugin("python.sphinx")
 
 
+description = "I am trying to learn Python"
+license = 'GNU GPL v3'
+version = '0.0.1'
 name = "mentoring"
-default_task = "publish"
+default_task = ['clean', 'analyze', 'publish']
+
+#default_task = "publish"
 
 
 @init
 def set_properties(project):
-    project.set_property_if_unset("coverage_threshold_warn", 95)
+    project.set_property('coverage_exceptions', ['run',])
+    project.set_property('coverage_branch_threshold_warn', 70)
+    project.set_property('coverage_branch_partial_threshold_warn', 70)
+    project.set_property('flake8_include_test_sources', True)
+    project.set_property('sphinx_builder', 'html')
--- src/unittest/python/json_db_tests.py
+++ src/unittest/python/json_db_tests.py
@@ -1,13 +1,16 @@
 import unittest
 import os
+import utils
 from json_db import JsonDbHandler, MyException
+
+my_dir = os.path.dirname(__file__)
+sample_file = os.path.join(my_dir, "sample.json")
+alt_file = os.path.join(my_dir, "any_other.json")
 
 
 class TestJsonDb(unittest.TestCase):
 
     def setUp(self):
-        my_dir = os.path.dirname(__file__)
-        sample_file = os.path.join(my_dir, "sample.json")
         self.op = JsonDbHandler(sample_file)
 
     def test_add(self):
@@ -41,6 +44,45 @@ class TestJsonDb(unittest.TestCase):
 
     def test_query_all(self):
         self.op.retrieve_all()
+
+    def test_save_default(self):
+        self.op.save(file=None)
+
+    def test_save_alt_file(self):
+        self.op.save(file=alt_file)
+
+    def test_base64ToString(self):
+        with open(sample_file, 'r') as f:
+            encoded_string = f.read()
+            decoded_string = utils._base64ToString(encoded_string)
+            self.assertIn(decoded_string,
+                          '{"Ragul": "Indian", "Ricardo": "Polaco", "Flamarion": "Brazilian"}')
+
+    def test_base64ToString_fail(self):
+        with open(sample_file, 'r') as f:
+            encoded_string = f.read()
+            decoded_string = utils._base64ToString(encoded_string)
+            self.assertNotIn(decoded_string, 'Z')
+
+    def test_stringToBase64(self):
+        string = '{"Ragul": "Indian", "Ricardo": "Polaco", "Flamarion": "Brazilian"}'
+        encoded_string = utils._stringToBase64(string)
+        self.assertIn(encoded_string,
+                      "eyJSYWd1bCI6ICJJbmRpYW4iLCAiUmljYXJkbyI6ICJQb2xhY28iLCAiRmxhbWFyaW9uIjogIkJyYXppbGlhbiJ9")
+
+    def test_stringToBase64_fail(self):
+        string = '{"Ragul": "Indian", "Ricardo": "Polaco", "Flamarion": "Brazilian"}'
+        encoded_string = utils._stringToBase64(string)
+        self.assertNotIn(encoded_string, "Z")
+
+    def test_read(self):
+        string = utils._read(sample_file)
+        self.assertIn(string,
+                      "eyJSYWd1bCI6ICJJbmRpYW4iLCAiUmljYXJkbyI6ICJQb2xhY28iLCAiRmxhbWFyaW9uIjogIkJyYXppbGlhbiJ9")
+
+    def test_read_fail(self):
+        string = utils._read(sample_file)
+        self.assertNotIn(string, "Z")
 
 
 if __name__ == '__main__':
